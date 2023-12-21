bbf97ee4b90f572535882f76e4b7795547029299
Abiammu77 <146917446+Abiammu77@users.noreply.github.com>
2023-12-21 10:46:53 +0530

    Add files via upload

---
+++ src/data/json.js
@@ -0,0 +1,134 @@
+const json = {
+    name: 'this pc',
+    isFolder: true,
+    items: [
+        {
+            name: 'Document',
+            isFolder: true,
+            items: [
+                {
+                    name: 'Document1.jpg',
+                    isFolder: false,
+                },
+                {
+                    name: 'Document2.jpg',
+                    isFolder: false,
+                },
+                {
+                    name: 'Document3.jpg',
+                    isFolder: false,
+                }
+            ]
+        },
+        {
+            name: 'Desktop',
+            isFolder: true,
+            items: [
+                {
+                    name: 'components',
+                    isFolder: true,
+                    items:[
+                        {
+                            name: 'Screenshot1.jpg',
+                            isFolder: false,
+                        },
+                        {
+                            name: 'videopal.mp4',
+                            isFolder: false,
+                        },
+                        
+                        
+                    ]
+                },
+                {
+                    name: 'index.js',
+                    isFolder: false,
+                },
+                {
+                    name: 'app.js',
+                    isFolder: false,
+                },
+                {
+                    name: 'package.json',
+                    isFolder: false,
+                }
+            ]
+        },
+        {
+            name: 'Downloads',
+            isFolder: true,
+            items: [
+                {
+                    name: 'Drivers',
+                    isFolder: true,
+                    items:[
+                        {
+                            name: 'Printerdriver.dmg',
+                            isFolder: false,
+                        },
+                        {
+                            name: 'cameradriver.dmg',
+                            isFolder: false,
+                        },
+                        
+                    ]
+                },
+                {
+                    name: 'index.js',
+                    isFolder: false,
+                },
+                {
+                    name: 'app.js',
+                    isFolder: false,
+                },
+                {
+                    name: 'package.json',
+                    isFolder: false,
+                }
+            ]
+        },
+        {
+            name: 'Application',
+            isFolder: true,
+            items: [
+                {
+                    name: 'components',
+                    isFolder: true,
+                    items:[
+                        {
+                            name: 'Webstorm.dmg',
+                            isFolder: false,
+                        },
+                        {
+                            name: 'Pycharm.dmg',
+                            isFolder: false,
+                        },
+                        {
+                            name: 'FileZila.dmg',
+                            isFolder: false,
+                        },
+                        {
+                            name: 'Mattermost.dmg',
+                            isFolder: false,
+                        },
+                        
+                    ]
+                },
+                {
+                    name: 'index.js',
+                    isFolder: false,
+                },
+                {
+                    name: 'app.js',
+                    isFolder: false,
+                },
+                {
+                    name: 'package.json',
+                    isFolder: false,
+                }
+            ]
+        }
+    ]
+
+};
+export default json;
